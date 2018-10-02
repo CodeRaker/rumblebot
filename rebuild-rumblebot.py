2ac544911a204499ff445da8ca9cdd2229fd4b2d
root <peterglad1985@hotmail.com>
2018-10-02 18:03:13 +0200

    sdf

---
+++ rebuild-rumblebot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+
+#pull
+os.system('git pull')
+
+#kill running process
+os.system("kill $(ps -f | grep -m 1 direct_communicate_madbot-madbot.py | awk '{ printf $2 }')")
+
+#build new image
+os.system('python3 /projects/rumblebot/containers/direct_communicate_madbot-madbot.py &')
