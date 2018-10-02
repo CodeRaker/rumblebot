e5324738db6521b9223c6750f3aceb92144aec28
DESKTOP-2RJFIAT\Peter <peterglad1985@hotmail.com>
2018-10-02 18:10:23 +0200

    d

--- rebuild-rumblebot.py
+++ rebuild-rumblebot.py
@@ -6,7 +6,7 @@ import os
 os.system('git pull')
 
 #kill running process
-os.system("kill $(ps -f | grep -m 1 direct_communicate_madbot-madbot.py | awk '{ printf $2 }')")
+os.system("kill $(ps -f | grep -m 1 rumblebot.py | awk '{ printf $2 }')")
 
 #build new image
-os.system('python3 /projects/rumblebot/containers/direct_communicate_madbot-madbot.py &')
+os.system('python3 /projects/rumblebot/rumblebot.py &')
--- rumblebot.py
+++ rumblebot.py
@@ -1,27 +1,27 @@
-import discord, os, subprocess, json, requests, math
+import discord
 
 client = discord.Client()
 ADMINS = []
 
 #Loads token from secrets file
 DISCORD_TOKEN = ''
-with open('/projects/stasi/containers/direct_communicate-secrets') as f:
+with open('/projects/rumblebot/rumblebot-secrets') as f:
     for l in f.read().split('\n'):
-        if 'sidekick_token' in l:
+        if 'rumblebot_token' in l:
             DISCORD_TOKEN = l.split(':')[1]
         if 'admin' in l:
             ADMINS.append(l.split(':')[1])
 
 #Grabs data from specified url
-def get_url(url):
-    r = requests.get(url)
-    return r
+#def get_url(url):
+#    r = requests.get(url)
+#    return r
 
 #Grabs the botserver public IP
-def get_public_ip():
-    public_ip = get_url('http://jsonip.com')
-    public_ip = public_ip.json()['ip']
-    return public_ip
+#def get_public_ip():
+#    public_ip = get_url('http://jsonip.com')
+#    public_ip = public_ip.json()['ip']
+#    return public_ip
 
 #Used for bot cleanup, checks if message is from bot
 def is_me(message):
@@ -32,12 +32,12 @@ def is_command(message):
     return str(message.content).split(' ')[0] in ['!hello']
 
 #run system command
-def command(system_command):
-    try:
-        CMD = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        return CMD.stdout, CMD.stderr
-    except Exception as e:
-        pass
+#def command(system_command):
+#    try:
+#        CMD = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+#        return CMD.stdout, CMD.stderr
+#    except Exception as e:
+#        pass
 
 #Actions bot takes on messages in Discord channel
 @client.event
@@ -71,74 +71,37 @@ async def on_message(message):
     if message.content == '!id':
         await client.send_message(message.channel, str(message.author.id))
 
-    if message.content == '!disconnect sidekick' and message.author.id in ADMINS:
+    if message.content == '!disconnect rumblebot' and message.author.id in ADMINS:
         await client.logout()
 
-    if message.content == '!rebuild sidekick' and message.author.id in ADMINS:
+    if message.content == '!rebuild rumblebot' and message.author.id in ADMINS:
         await client.logout()
-        os.system('/projects/stasi/containers/rebuild-dc_sidekick.py &')
-
-    #Run local command
-    if message.content.startswith('!sidekick') and message.author.id in ADMINS:
-        try:
-            system_command = message.content.replace('!sidekick ', '')
-            c = command(system_command)
-            stdout = c[0].read().decode("utf-8")
-            stderr = c[1].read().decode("utf-8")
-
-            #regular length message
-            #stdout
-            if stdout and len(stdout) < 2000:
-                await client.send_message(message.channel, "stdout\n```bash\n" + stdout + "```")
-            #stderr
-            if stderr and len(stderr) < 2000:
-                await client.send_message(message.channel, "stderr\n```bash\n" + stderr + "```")
-
-            #length exceeds discord message limit
-            #stdout
-            if stdout and len(stdout) > 2000:
-                count = math.ceil(len(stdout) / 1950)
-                for i in range(0, count):
-                    start = i*1950
-                    end = (i+1)*1950
-                    await client.send_message(message.channel, "stdout\n```bash\n" + stdout[start:end] + "```")
-            #stderr
-            if stderr and len(stderr) > 2000:
-                count = math.ceil(len(stderr) / 1950)
-                for i in range(0, count):
-                    start = i*1950
-                    end = (i+1)*1950
-                    await client.send_message(message.channel, "stderr\n```bash\n" + stderr[start:end] + "```")
-
-        #Logs exception
-        except Exception as e:
-            pass
+        os.system('/projects/rumblebot/rebuild-rumblebot.py &')
 
 #    if message.content == '!play' and message.author.id in botadmins:
 #        client.change_status(discord.Game(name='Tetris'))
 
     #Print help/commands menu
-    if message.content in ['!commands sidekick','!help']:
+    if message.content in ['!commands','!help']:
         embed = discord.Embed(title='Control', description='Commands for controlling STASI', colour=0xDEADBF)
         embed.add_field(name="Command List", value="""
 ```bash
 !commands               # Shows this menu
-!system <bash command>  # Run host command
 ```""")
         await client.send_message(message.channel, embed=embed)
 
 #Creates a background stats update task
-async def update_task():
-    await client.wait_until_ready()
-    while not client.is_closed:
-        try:
+#async def update_task():
+#    await client.wait_until_ready()
+#    while not client.is_closed:
+#        try:
             #Update user and statistics data and wait 1 hour
-            player.initialize()
-            await asyncio.sleep(3600)
+#            player.initialize()
+#            await asyncio.sleep(3600)
 
         #Logs exception
-        except Exception as e:
-            os.system('echo "' + str(datetime.datetime.now()) + " Update_task: " + str(e) + '" >> ' + logpath)
+#        except Exception as e:
+#            os.system('echo "' + str(datetime.datetime.now()) + " Update_task: " + str(e) + '" >> ' + logpath)
 
 #Startup code
 #client.loop.create_task(update_task())
